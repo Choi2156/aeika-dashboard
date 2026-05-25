1bce58ea6019cfe8b27fbcc7cf6148ccd2c462fb
Choi5903 <cmg9431@gmail.com>
2026-05-25 14:28:07 +0900

    fix: replace mail icon button with wider text pill (no link)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -38,13 +38,10 @@ export default function Footer({ onOpenLicense }) {
                 <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
               </svg>
             </a>
-            <a
-              href="mailto:choi21mg@gmail.com"
-              className="footer-social-btn footer-social-btn--mail"
-              title="관리자에게 문의/제보 메일 보내기"
-            >
-              <Mail size={16} />
-            </a>
+            <span className="footer-social-btn footer-social-btn--mail footer-social-btn--mail-text">
+              <Mail size={14} />
+              <span>choi21mg@gmail.com</span>
+            </span>
           </div>
         </div>
 
